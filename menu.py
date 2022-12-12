d543b8253f1a2c1402d90642529a30c11e99c591
Marco Macca <47415507+marcomacca@users.noreply.github.com>
2022-12-12 21:15:25 +0100

    Add files via upload

--- menu.py
+++ menu.py
@@ -15,20 +15,20 @@ class Menu:
                              onclose=pygame_menu.events.CLOSE,enabled=False,
                              title='Traffic Simulator')
         #self.menu.add_text_input('Name: ', default='John Doe')
-        #self.menu.add_selector('Difficulty: ', [('Hard', 1), ('Easy', 2)],)
-        self.menu.add_button('Play' ,pygame_menu.events.CLOSE)
-        self.menu.add_button('Reset', self.reset)
-        self.menu.add_button('Graph', self.create_graph)
-        self.menu.add_button('Overview', self.create_submenu)
-        self.menu.add_selector('Smart Traffic: ',
+        #self.menu.add.selector('Difficulty: ', [('Hard', 1), ('Easy', 2)],)
+        self.menu.add.button('Play' ,pygame_menu.events.CLOSE)
+        self.menu.add.button('Reset', self.reset)
+        self.menu.add.button('Graph', self.create_graph)
+        self.menu.add.button('Overview', self.create_submenu)
+        self.menu.add.selector('Smart Traffic: ',
                            [('ON',True),
                             ('OFF',False)],
                            onchange=self.smart_traffic_controller,default=1)
-        self.menu.add_selector('Debug: ',
+        self.menu.add.selector('Debug: ',
                            [('ON',True),
                             ('OFF',False)],default=1,
                            onchange=self.debug)
-        self.menu.add_button('Quit', self.close)
+        self.menu.add.button('Quit', self.close)
         #self.menu.add_image(self.game.car_path)
         self.menu.set_relative_position(10, 5)
         game.menu = self.menu
@@ -43,8 +43,8 @@ class Menu:
          'Tempo trascorso: {0}'.format(round(pygame.time.get_ticks()/1000),2),
          'Media percorrenza: {0}'.format(round(statistics.mean(self.game.life),2) if len(self.game.life) != 0 else 0),'', ]
         for m in TEXT:
-            self.submenu.add_label(m, align=pygame_menu.locals.ALIGN_LEFT, font_size=30)
-        self.submenu.add_button('Return to main menu', pygame_menu.events.RESET)
+            self.submenu.add.label(m, align=pygame_menu.locals.ALIGN_LEFT, font_size=30)
+        self.submenu.add.button('Return to main menu', pygame_menu.events.RESET)
         self.submenu.set_relative_position(20, 10)
         self.menu._open(self.submenu)
             
@@ -97,17 +97,17 @@ class Menu:
     #         title='Submenu',
     #         width=400,)
     #     for i in range(30):
-    #         play_submenu.add_button('Back {0}'.format(i), pygame_menu.events.BACK)
-    #     play_submenu.add_button('Return to main menu', pygame_menu.events.RESET)
+    #         play_submenu.add.button('Back {0}'.format(i), pygame_menu.events.BACK)
+    #     play_submenu.add.button('Return to main menu', pygame_menu.events.RESET)
 
-    #     play_menu.add_button('Start',  # When pressing return -> play(DIFFICULTY[0], font)
+    #     play_menu.add.button('Start',  # When pressing return -> play(DIFFICULTY[0], font)
     #                          pygame_menu.events.CLOSE,
     #                          pygame.font.Font(pygame_menu.font.FONT_FRANCHISE, 30))
-    #     #play_menu.add_selector('Select difficulty ',
+    #     #play_menu.add.selector('Select difficulty ',
     #     #                       [('1 - Easy', 'EASY'),
     #     #                        ('2 - Medium', 'MEDIUM'),
     #     #                        ('3 - Hard', 'HARD')],
     #     #                       onchange=change_difficulty,
     #     #                       selector_id='select_difficulty')
-    #     #play_menu.add_button('Another menu', play_submenu)
-    #     play_menu.add_button('Return to main menu', pygame_menu.events.BACK)
+    #     #play_menu.add.button('Another menu', play_submenu)
+    #     play_menu.add.button('Return to main menu', pygame_menu.events.BACK)
